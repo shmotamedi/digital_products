4b093550fc96a1eaa2fbb950354bdea6d182b212
shahryar motamedi <shahryar.motamedi@yahoo.com>
2023-12-17 09:03:32 +0330

    complete gitignore of project

--- products/models.py
+++ products/models.py
@@ -4,19 +4,19 @@ class Category(models.Model):
     parent=models.ForeignKey('self',blank=True,null=True,on_delete=models.CASCADE)
     title=models.CharField(max_length=50)
     description=models.TextField(blank=True)
-    avator=models.ImageField(blank=True,upload_to='Categories/')
+    avatar=models.ImageField(blank=True,upload_to='Categories/')
     is_enable=models.BooleanField(default=True)
     created_time=models.DateTimeField(auto_now_add=True)
     updated_time=models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table='Categgories'
+        db_table='Categories'
         verbose_name='Category'
         verbose_name_plural='Categories'
 class Product(models.Model):
     title=models.CharField(max_length=50)
     description=models.TextField(blank=True)
-    avator=models.ImageField(blank=True,upload_to='Products/')
+    avatar=models.ImageField(blank=True,upload_to='Products/')
     is_enable=models.BooleanField(default=True)
     categories = models.ManyToManyField('Category', blank=True)
     created_time=models.DateTimeField(auto_now_add=True)
@@ -30,7 +30,7 @@ class File(models.Model):
     product=models.ForeignKey('Product',on_delete=models.CASCADE)
     title=models.CharField(max_length=50)
     file=models.FileField(upload_to='files/%Y/%m/%d')
-    avatar=models.ImageField(blank=True,upload_to='Products')
+    avatar=models.ImageField(blank=True,upload_to='Products/')
     is_enable=models.BooleanField(default=True)
     created_time=models.DateTimeField(auto_now_add=True)
     updated_time=models.DateTimeField(auto_now=True)
